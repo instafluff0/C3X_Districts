9492919e0dbe32ae8d1eddf068ac0952646fa1c2
Insta Fluff <instafluff0@gmail.com>
2026-02-27 17:02:41 -0800

    Apply downstream changes on top of upstream master

--- Civ3Conquests.h
+++ Civ3Conquests.h
@@ -772,6 +772,12 @@ enum Unit_Command_Values
   UCV_Build_WonderDistrict = -10000002,
   UCV_Build_DistributionHub = -10000003,
   UCV_Build_Aerodrome = -10000004,
+  UCV_Build_Port = -10000005,
+  UCV_Build_CentralRailHub = -10000006,
+  UCV_Build_EnergyGrid = -10000007,
+  UCV_Build_Bridge = -10000008,
+  UCV_Build_Canal = -10000009,
+  UCV_Build_GreatWall = -10000010,
 };
 
 enum Unit_Mode_Actions
@@ -2081,10 +2087,10 @@ struct Map_vtable
   bool (__fastcall * check_goody_hut_location) (Map *, __, int, int);
   byte (__fastcall * m19_Create_Tiles)(Map * this, __, Tile ** out_array);
   int m20;
-  byte (__fastcall * is_near_lake) (Map * this, __, int x, int y, int num_tiles);
-  byte (__fastcall * is_near_river) (Map * this, __, int x, int y, int num_tiles);
+  bool (__fastcall * is_near_lake) (Map * this, __, int x, int y, int num_tiles);
+  bool (__fastcall * is_near_river) (Map * this, __, int x, int y, int num_tiles);
   int m23;
-  int (__fastcall * has_fresh_water) (Map * this, __, int x, int y);
+  bool (__fastcall * has_fresh_water) (Map * this, __, int x, int y);
   int m25;
   int m26;
   int m27;
